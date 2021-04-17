d381df5518eb381ea68102d18a4ed2bf51edd7f5
TheHawk3r <75886041+TheHawk3r@users.noreply.github.com>
2021-04-17 23:53:56 +0300

    Update settings.py

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'cin5marian@gmail.com' 
-EMAIL_HOST_PASSWORD = 'rupenorma5'
-EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your@mail.com' 
+EMAIL_HOST_PASSWORD = 'Your apssword'
+EMAIL_PORT = smtp port
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
